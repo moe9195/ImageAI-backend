44f9be1d6f52a294e9ea32536b9c0671500379a5
moe9195 <moerahmeh95@hotmail.com>
2020-05-04 22:37:52 +0300

    updated requirements

--- ImageAI/views.py
+++ ImageAI/views.py
@@ -49,86 +49,92 @@ class UserDetails(RetrieveAPIView):
         return User.objects.filter(id = self.request.user.id)
 
 class GiveKey(views.APIView):
-    permission_classes = [IsAuthenticated]
-    def post(self , request , *args , **kwargs):
-        name = request.data.get("name")
-        api_key, key = APIKey.objects.create_key(name=name ,expiry_date =datetime.now()+timedelta(days=30) )
-        profile = Profile.objects.get(user = self.request.user)
-        profile.limit += 60
-        profile.subscribed = True
-        profile.key= key
-        profile.save()
-        return Response(key , status=status.HTTP_201_CREATED)
+
+	permission_classes = [IsAuthenticated]
+
+	def post(self, request, *args, **kwargs):
+		name = request.data.get("name")
+		api_key, key = APIKey.objects.create_key(name=name ,expiry_date =datetime.now()+timedelta(days=30) )
+		profile = Profile.objects.get(user = self.request.user)
+		profile.limit += 60
+		profile.subscribed = True
+		profile.key= key
+		profile.save()
+		return Response(key , status=status.HTTP_201_CREATED)
 
 
 class Processing(views.APIView):
-    def post(self, request, *args, **kwargs):
-        img=request.data.get("img")
-        method = request.data.get("method")
 
-        # If the POSTED image is a string
-        if isinstance(img, str):
-            # check if it is base64
-            if 'base64' in img[10:30]:
-                img_b64 = img.split(',', 1)[1]
-                img = np.array(Image.open(io.BytesIO(base64.b64decode(img_b64))))
-            # check if it is a url
-            else:
-                response = requests.get(img)
-                img = np.array(Image.open(io.BytesIO(response.content)))
+	permission_classes =[HasAPIKey]
 
-        # If POSTED image is a file
-        else:
-            img = np.array(Image.open(io.BytesIO(img.file.read())))
+	def post(self, request, *args, **kwargs):
 
-        # if image only has 1 channel convert it to 3 channels RGB
-        if len(img.shape) == 2:
-            img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+	    img=request.data.get("img")
+	    method = request.data.get("method")
 
-        # In case of png with alpha channel
-        if len(img.shape) > 2 and img.shape[2] == 4:
-            #convert the image from RGBA2RGB
-            img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+	    # If the POSTED image is a string
+	    if isinstance(img, str):
+	        # check if it is base64
+	        if 'base64' in img[10:30]:
+	            img_b64 = img.split(',', 1)[1]
+	            img = np.array(Image.open(io.BytesIO(base64.b64decode(img_b64))))
+	        # check if it is a url
+	        else:
+	            response = requests.get(img)
+	            img = np.array(Image.open(io.BytesIO(response.content)))
 
-        # In case image is black and white and has alpha channel
-        if len(img.shape) == 2 and img.shape[2] == 4:
-            # convert the image from RGBA2RGB
-            img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+	    # If POSTED image is a file
+	    else:
+	        img = np.array(Image.open(io.BytesIO(img.file.read())))
 
-        # run each algorithm based on method and return processed img
-        if method == "SuperResolution":
-            print("HERE")
-            print(img.shape)
-            print(np.prod(img.shape))
-            im = super_resolution(img)
+	    # if image only has 1 channel convert it to 3 channels RGB
+	    if len(img.shape) == 2:
+	        img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 
-        elif method == "Colorize":
-            im = colorize(img)
+	    # In case of png with alpha channel
+	    if len(img.shape) > 2 and img.shape[2] == 4:
+	        #convert the image from RGBA2RGB
+	        img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
 
-        elif method == "DeepArt":
-            style = request.data.get("style")
-            if style:
-                im = deep_art(img, style)
-                # if style selected is invalid return the error string
-                if isinstance(im, str):
-                    return Response(im, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-            # if no style is selected, default to wave
-            else:
-                im = deep_art(img, "wave")
+	    # In case image is black and white and has alpha channel
+	    if len(img.shape) == 2 and img.shape[2] == 4:
+	        # convert the image from RGBA2RGB
+	        img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
 
-        elif method == "Deblur":
-            im = deblur(img)
+	    # run each algorithm based on method and return processed img
+	    if method == "SuperResolution":
+	        print("HERE")
+	        print(img.shape)
+	        print(np.prod(img.shape))
+	        im = super_resolution(img)
 
-        elif method == "Classify":
-            # in the case of classification, we return an object instead of an image
-            obj = classify(img)
-            return Response(obj, status=status.HTTP_201_CREATED)
+	    elif method == "Colorize":
+	        im = colorize(img)
 
-        else:
-            pass
+	    elif method == "DeepArt":
+	        style = request.data.get("style")
+	        if style:
+	            im = deep_art(img, style)
+	            # if style selected is invalid return the error string
+	            if isinstance(im, str):
+	                return Response(im, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+	        # if no style is selected, default to wave
+	        else:
+	            im = deep_art(img, "wave")
 
-        # The image is then encoded to base64 and returned to the request user
-        buffered =  io.BytesIO()
-        im.save(buffered, format="JPEG")
-        encoded_img = base64.b64encode(buffered.getvalue())
-        return Response(encoded_img, status=status.HTTP_201_CREATED)
+	    elif method == "Deblur":
+	        im = deblur(img)
+
+	    elif method == "Classify":
+	        # in the case of classification, we return an object instead of an image
+	        obj = classify(img)
+	        return Response(obj, status=status.HTTP_201_CREATED)
+
+	    else:
+	        pass
+
+	    # The image is then encoded to base64 and returned to the request user
+	    buffered =  io.BytesIO()
+	    im.save(buffered, format="JPEG")
+	    encoded_img = base64.b64encode(buffered.getvalue())
+	    return Response(encoded_img, status=status.HTTP_201_CREATED)
